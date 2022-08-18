5785d043b2b8aa165ea2331749ee525f7b4f292d
yuyu <hcsd7b34@gmail.com>
2022-08-18 10:43:36 +0900

    Fix: index.d.tsを更新

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,6 +1,6 @@
 type dataType = 'global' | 'guild' | 'user' | 'dm';
 type operatorString = '+' | '-' | '*' | '/' | '%' | '**' | '&&' | '||' | '??';
 
-export function getData(type: dataType, id: string | null, path: string[]): Object | null
-export function setData(type: dataType, id: string | null, path: string[], value: any, calc?: operatorString | ((old: Object | null, val: Object | null) => Object | null) | null): void
-export function deleteData(type: dataType, id: string | null, path: string[]): void
+export function getData(type: dataType, id: string | null, path: string[], useCache?: boolean): Object | null
+export function setData(type: dataType, id: string | null, path: string[], value: any, calc?: operatorString | ((old: Object | null, val: Object | null) => Object | null) | null, useCache?: boolean): void
+export function deleteData(type: dataType, id: string | null, path: string[], useCache?: boolean): void
